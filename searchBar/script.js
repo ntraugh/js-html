c41c62c83d54d2981d6196acd55df42bb2103ae6
Nate Traugh <n.traugh@yahoo.com>
2022-09-14 11:49:24 -0400

    changed forEach to map to return an object with name, email and the card. looping through users in our event listener to check if their name includes that value, if it does we'll show the card if not it won't show

--- searchBar/script.js
+++ searchBar/script.js
@@ -3,6 +3,7 @@ const userCardTemplate = document.querySelector("[user-data-template]")
 const userCardContainer = document.querySelector("[data-user-cards-container]")
 
 
+let users = []
 
 
 fetch("https://jsonplaceholder.typicode.com/users")
@@ -10,20 +11,22 @@ fetch("https://jsonplaceholder.typicode.com/users")
 .then(data => {
     
     // after our forEach statement we are given 10 cards
-    data.forEach(user => {
+    users = data.map(user => {
         const card = userCardTemplate.content.cloneNode(true).children[0]
         const header = card.querySelector("[data-header]")
         const body = card.querySelector("[data-body]")
         header.textContent = user.name
         body.textContent = user.email
         userCardContainer.append(card)
-        
+        return { name: user.name, email: user.email, element: card}
     })
     
 })
 
 
-searchInput.addEventListener("change", e => {
+searchInput.addEventListener("input", e => {
     const value = e.target.value
-    console.log(value)
+    users.forEach(user => {
+        const isVisible = user.name.includes(value) || user.email.includes(value)
+    })
 })
