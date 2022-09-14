6821a62299d5d3f95d4f137e9dee2b25747f1119
Nate Traugh <n.traugh@yahoo.com>
2022-09-14 11:42:54 -0400

    fetched data successfully and display it on the page but getting addEventListener error

--- searchBar/script.js
+++ searchBar/script.js
@@ -1,9 +1,29 @@
+const searchInput = document.querySelector("[data-search]")
 const userCardTemplate = document.querySelector("[user-data-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+
+
+
 
 fetch("https://jsonplaceholder.typicode.com/users")
 .then(res => res.json())
 .then(data => {
-    const card = userCardTemplate.content.cloneNode(true)
-    console.log(card)
+    
+    // after our forEach statement we are given 10 cards
+    data.forEach(user => {
+        const card = userCardTemplate.content.cloneNode(true).children[0]
+        const header = card.querySelector("[data-header]")
+        const body = card.querySelector("[data-body]")
+        header.textContent = user.name
+        body.textContent = user.email
+        userCardContainer.append(card)
+        
+    })
+    
+})
 
+
+searchInput.addEventListener("change", e => {
+    const value = e.target.value
+    console.log(value)
 })
