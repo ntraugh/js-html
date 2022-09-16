fe6ec1fd11ed2c127c3e1b17497c3a318d549db9
Nate Traugh <n.traugh@yahoo.com>
2022-09-16 11:10:13 -0400

    just playing around with javascript changing the background color on click

--- website/script.js
+++ website/script.js
@@ -2,8 +2,9 @@ const products = document.getElementById("products")
 const trial = document.getElementById("trial")
 
 const handleClick = () => {
-    console.log("clicked")
-
+    const hero = document.querySelector(".hero")
+    hero.style.backgroundColor = "salmon"
+    
 }
 
 
