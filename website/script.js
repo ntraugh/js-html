945823c7dd5e48a3756639d52ece3c83eacd13a0
Nate Traugh <n.traugh@yahoo.com>
2022-09-16 12:47:50 -0400

    getting the last user to populate in a div, not sure why the others aren't populating yet

--- website/script.js
+++ website/script.js
@@ -1,6 +1,27 @@
 const products = document.getElementById("products")
 const trial = document.getElementById("trial")
 
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(data => displayData(data))
+
+
+const displayData = (data) => {
+   console.log(data)
+    data.map((user) => {
+       const {name, email} = user
+       const div = `<div class="user">
+       <h1 class="name">
+           ${name}
+       </h1>
+       <p class="email">
+           ${email}
+       </p>
+   </div>`
+   const oldDiv = document.querySelector(".users-grid")
+   oldDiv.innerHTML = div
+    })
+}
 const handleClick = () => {
     const hero = document.querySelector(".hero")
     hero.style.backgroundColor = "salmon"
