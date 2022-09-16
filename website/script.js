baee3f9426a68c6cb6a4dac19e80b35b10b6e6f7
Nate Traugh <n.traugh@yahoo.com>
2022-09-16 11:32:57 -0400

    made the hero section responsive enough for now, want to try and keep messing around with javascript, fetching data from jsonPlaceholder API to populate user cards

--- website/script.js
+++ website/script.js
@@ -4,9 +4,17 @@ const trial = document.getElementById("trial")
 const handleClick = () => {
     const hero = document.querySelector(".hero")
     hero.style.backgroundColor = "salmon"
-    
+}
+
+const handleAnotherClick = () => {
+    const hero = document.querySelector(".hero")
+    hero.style.backgroundColor = "cadetblue"
+    // window.location.replace("https://www.google.com")
 }
 
 
 
+
+
 products.addEventListener("click", handleClick)
+trial.addEventListener("click", handleAnotherClick)
